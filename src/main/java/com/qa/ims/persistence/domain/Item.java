9e17df26a36ac74f9fe4c8794380192fe7f65d72
AWalker2112 <AWalker@qa.com>
2021-02-01 17:26:53 +0000

    items functionality

---
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -0,0 +1,87 @@
+package com.qa.ims.persistence.domain;
+
+public class Item {
+
+	private Long itemID;
+	private String itemName;
+	private Double value;
+
+	public Item(long itemID, String itemName, double value) {
+
+		this.itemID = itemID;
+		this.itemName = itemName;
+		this.value = value;
+	}
+
+	public Item(String itemName, double value) {
+
+		this.itemName = itemName;
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return "itemID:" + itemID + ", itemName:" + itemName + ", value:" + value + "]";
+	}
+
+	public long getItemID() {
+		return itemID;
+	}
+
+	public void setItemID(long itemID) {
+		this.itemID = itemID;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((itemID == null) ? 0 : itemID.hashCode());
+		result = prime * result + ((itemName == null) ? 0 : itemName.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (itemID == null) {
+			if (other.itemID != null)
+				return false;
+		} else if (!itemID.equals(other.itemID))
+			return false;
+		if (itemName == null) {
+			if (other.itemName != null)
+				return false;
+		} else if (!itemName.equals(other.itemName))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+}
--- src/test/java/com/qa/ims/persistence/domain/CustomerTest.java
+++ src/test/java/com/qa/ims/persistence/domain/CustomerTest.java
@@ -1,14 +1,75 @@
 package com.qa.ims.persistence.domain;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 
 public class CustomerTest {
-
+	
+	private Customer customer1 = new Customer("Alex", "Walker");
+	private Customer customer2 = new Customer(3l, "James", "Leek");
+	
+	
 	@Test
 	public void testEquals() {
 		EqualsVerifier.simple().forClass(Customer.class).verify();
 	}
 
+	
+	@Test
+	public void getIDTest() {
+		
+		assertEquals(3l,customer2.getId(),0);
+		
+	}
+	
+	@Test
+	public void setIDTest() {
+		
+		customer2.setId(4l);
+		assertEquals(4l,customer2.getId(),0);
+		
+	}
+	
+	@Test
+	public void getFirstNameTest() {
+		
+		assertEquals("Alex",customer1.getFirstName());
+		
+	}
+	
+	@Test
+	public void setFirstNameTest() {
+		
+		customer1.setFirstName("Ben");
+		
+		assertEquals("Ben",customer1.getFirstName());
+		
+	}
+	
+	@Test
+	public void getSurnameTest() {
+		
+		assertEquals("Walker",customer1.getSurname());
+		
+	}
+	
+	@Test
+	public void setSurnameTest() {
+		
+		customer1.setSurname("Smith");
+		
+		assertEquals("Smith",customer1.getSurname());
+		
+	}
+	
+	@Test
+	public void toStringTest() {
+		
+		System.out.println(customer2.toString());
+		assertEquals("id:3 first name:James surname:Leek", customer2.toString());
+	}
+	
 }
