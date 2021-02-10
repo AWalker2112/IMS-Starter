9c07cdb80450b11656666fe4ab1ba79b0343abc9
AWalker2112 <AWalker@qa.com>
2021-02-10 11:53:06 +0000

    QIS-72 #comment updated fat .jar file

---
+++ target/classes/sql-schema.sql
@@ -0,0 +1,38 @@
+drop schema `ims`;
+
+CREATE SCHEMA IF NOT EXISTS `ims`;
+
+USE `ims` ;
+
+CREATE TABLE IF NOT EXISTS `customers` (
+    `customer_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(40) DEFAULT NULL,
+    `surname` VARCHAR(40) DEFAULT NULL,
+    PRIMARY KEY (`customer_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `items` (
+    `item_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) DEFAULT NULL,
+    `item_value` DECIMAL(10,2) DEFAULT NULL,
+    PRIMARY KEY (`item_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `orders` (
+    `order_id` INT(11) NOT NULL AUTO_INCREMENT,
+    
+    `fk_customer_id` INT(11) NOT NULL,
+    PRIMARY KEY (`order_id`),
+    FOREIGN KEY (`fk_customer_id`) REFERENCES `customers`(`customer_id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `orders_items` (
+    `order_items_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `fk_order_id` INT(11) NOT NULL,
+    `fk_item_id` INT(11) NOT NULL,
+    `quantity` INT NOT NULL,
+    PRIMARY KEY (`order_items_id`),
+    FOREIGN KEY (`fk_order_id`) REFERENCES `orders`(`order_id`),
+    FOREIGN KEY (`fk_item_id`) REFERENCES `items`(`item_id`)
+);
