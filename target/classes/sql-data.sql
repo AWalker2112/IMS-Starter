84427f541723e2b98b8cac7af3f6c5a5f1799eb2
AWalker2112 <AWalker@qa.com>
2021-02-12 10:39:27 +0000

    QIS-75 #comment presentation upload

--- target/classes/sql-data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
-
-INSERT INTO `items` (`item_name`, `item_value`) VALUES ('iphone', '399.99');
-
-INSERT INTO `orders` (`fk_customer_id`) VALUES (1);
